f16b3655befa6add66c2c2e99b5099a5aaae3691
aniket nimkar <aniketnimkar88@gmail.com>
2026-06-24 17:57:30 +0530

    updte code

--- src/day2/Program3_DataTypeExample.java
+++ src/day2/Program3_DataTypeExample.java
@@ -4,7 +4,24 @@ public class Program3_DataTypeExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//8byte
+		long phoneNumber = 9483495930L;
+		System.out.println(phoneNumber);
+		
+		//4byte 7 floating pts
+		float marks1=98.123456789F;
+		System.out.println(marks1);
+		
+		float marks2 = 9857.123456789F;
+		System.out.println(marks2);
+		
+		//8 byte 15 floating point
+		double data3= 1234.123456789012345;
+		System.out.println(data3);
 
+		
+		
 	}
 
 }
--- src/day2/Program5_TypeCasting.java
+++ src/day2/Program5_TypeCasting.java
@@ -4,7 +4,18 @@ public class Program5_TypeCasting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//Downcasting or Explicit type conversion
+		
+		float height = 162.5f;
+		int heightInt = (int)height;
+		System.out.println(heightInt);
+		
+		
+		//Upcasting = IMPLICIT TYPE conversion
+		int age=32;
+		float ageFloat = age;
+		System.out.println(ageFloat);
 	}
 
 }
